68ded226d87c81902f4d7f0e2868516ce0184518
Jolanta Ślipko <j.slipko21@gmail.com>
2021-07-12 12:15:54 +0200

    Add copy color feature

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,16 +1,22 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import * as Styled from "./ColorBox.styles";
 
 const ColorBox = ({ color, name }) => {
+  const handleCopy = () => {
+    console.log("copied");
+  };
   return (
-    <Styled.ColorBox bgColor={color}>
-      <Styled.CopyContainer>
-        <Styled.CopyButton>COPY</Styled.CopyButton>
-      </Styled.CopyContainer>
-      <Styled.InfoContainer>
-        <span className="colorbox__name">{name}</span>
-        <Styled.Button>MORE</Styled.Button>
-      </Styled.InfoContainer>
-    </Styled.ColorBox>
+    <CopyToClipboard text={color} onCopy={handleCopy}>
+      <Styled.ColorBox bgColor={color}>
+        <Styled.CopyContainer>
+          <Styled.CopyButton>COPY</Styled.CopyButton>
+        </Styled.CopyContainer>
+        <Styled.InfoContainer>
+          <span className="colorbox__name">{name}</span>
+          <Styled.Button>MORE</Styled.Button>
+        </Styled.InfoContainer>
+      </Styled.ColorBox>
+    </CopyToClipboard>
   );
 };
 
