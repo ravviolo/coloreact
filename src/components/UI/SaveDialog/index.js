b9e7727baead76905df77af7dc7ed64c97d57069
Jolanta Ślipko <j.slipko21@gmail.com>
2021-07-15 15:36:11 +0200

    Add save dialog form validation

--- src/components/UI/SaveDialog/index.js
+++ src/components/UI/SaveDialog/index.js
@@ -1,5 +1,7 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import UIContext from "../../../context/UIContext";
+import PaletteContext from "../../../context/PaletteContext";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -10,49 +12,67 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 const SaveDialog = () => {
-  const {openSaveDialog, setOpenSaveDialog} = useContext(UIContext)
+  const { openSaveDialog, setOpenSaveDialog } = useContext(UIContext);
+  const { palettes } = useContext(PaletteContext);
+  const [enteredName, setEnteredName] = useState("");
 
   const handleClose = () => {
     setOpenSaveDialog(false);
   };
-  const handleNext = () => {
+  const handleSubmit = () => {
     setOpenSaveDialog(false);
-  //open emoji dialog
-  }
+    //open emoji dialog
+  };
+  const handleInputChange = (e) => setEnteredName(e.target.value);
+
+  useEffect(() => {
+    ValidatorForm.addValidationRule("isNameUnique", () => {
+      return palettes.every(
+        ({ paletteName }) =>
+          paletteName.toLowerCase() !== enteredName.toLowerCase()
+      );
+    });
+  });
 
   return (
     <div>
-      {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open form dialog
-      </Button> */}
       <Dialog
         open={openSaveDialog}
         onClose={handleClose}
         aria-labelledby="save-modal-title"
       >
-        <DialogTitle id="save-modal-title">Choose a Palette Name</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            You have chosen some lovely colors!
-            <br />
-            Time to give this palette a proper name. Make sure it's unique!
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Palette Name"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleNext} color="primary">
-            Next
-          </Button>
-        </DialogActions>
+        <ValidatorForm onSubmit={handleSubmit} instantValidate={false}>
+          <DialogTitle id="save-modal-title">Choose a Palette Name</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              You have chosen some lovely colors!
+              <br />
+              Time to give this palette a proper name. Make sure it's unique!
+            </DialogContentText>
+            <TextValidator
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Palette Name"
+              fullWidth
+              value={enteredName}
+              onChange={handleInputChange}
+              validators={["required", "isNameUnique"]}
+              errorMessages={[
+                "Enter palette name",
+                "Palette name already taken",
+              ]}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button color="primary" variant="contained" type="submit">
+              Next
+            </Button>
+          </DialogActions>
+        </ValidatorForm>
       </Dialog>
     </div>
   );
