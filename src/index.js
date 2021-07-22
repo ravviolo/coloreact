d40d659e177b102082a79b8d6fde215cb0b1f8a5
Jolanta Ślipko <j.slipko21@gmail.com>
2021-07-22 16:22:20 +0200

    Add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <PaletteContextProvider>
         <UIContextProvider>
           <App />
