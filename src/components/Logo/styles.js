6413e0b097dd9f2d4b08ae156ddbead6864f9e44
Jolanta Ślipko <j.slipko21@gmail.com>
2021-07-22 17:15:05 +0200

    Change logo styles for small screens

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme) => ({
   "@keyframes rainbow": {
     "0%": { backgroundPosition: "0% 82%" },
     "50%": { backgroundPosition: "100% 19%" },
@@ -12,6 +12,18 @@ export const useStyles = makeStyles({
     letterSpacing: 5,
 
     "&:hover": {
+      animationName: "$rainbow",
+      animationDuration: "6s",
+      animationIterationCount: "infinite",
+      animationTimingFunction: "ease",
+    },
+    [theme.breakpoints.down("md")]: {
+      background:
+        "linear-gradient(124deg,#1ddde8,#1d64e8,#2b1de8, #641de8,#dd00f3,#b003d3,#cf07c5,#f300df,#ff2400,#e81d1d,#e8b71d,#e3e81d,#1de840)",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+      backgroundSize: "400% 100%",
       animationName: "$rainbow",
       animationDuration: "6s",
       animationIterationCount: "infinite",
@@ -34,6 +46,19 @@ export const useStyles = makeStyles({
       color: "transparent",
       backgroundSize: "400% 100%",
     },
+
+    [theme.breakpoints.down("sm")]: {
+      background:
+        "linear-gradient(124deg,#1ddde8,#1d64e8,#2b1de8, #641de8,#dd00f3,#b003d3,#cf07c5,#f300df,#ff2400,#e81d1d,#e8b71d,#e3e81d,#1de840)",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      color: "transparent",
+      backgroundSize: "400% 100%",
+      animationName: "$rainbow",
+      animationDuration: "6s",
+      animationIterationCount: "infinite",
+      animationTimingFunction: "ease",
+    },
   },
   Secondary: {
     fontSize: "1.5rem",
@@ -52,4 +77,4 @@ export const useStyles = makeStyles({
       width: "100%",
     },
   },
-});
+}));
